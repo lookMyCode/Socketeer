b99aa05b55026affee80e8ab54d62896a69a00cc
Dmytro Medynskyi <dimasik22101993@gmail.com>
2026-02-11 15:36:09 +0100

    feat/chats-controller-base-example

--- src/example/ChatsController.ts
+++ src/example/ChatsController.ts
@@ -1,6 +1,30 @@
-import { Controller } from "../controller";
+import { Controller, OnSocketConnect } from "../controller";
+import { SocketContext } from "../SocketContext";
 
 
-export class ChatsController extends Controller {
+export class ChatsController extends Controller implements OnSocketConnect {
 
+  private chats = [
+    {
+      id: 1,
+      name: 'First',
+    },
+    {
+      id: 2,
+      name: 'Second',
+    },
+    {
+      id: 3,
+      name: 'Thierd',
+    },
+  ];
+
+  async $onSocketConnect(context: SocketContext<any>) {
+    await this.$send(context, {
+      event: 'connect',
+      payload: {
+        list: this.chats
+      }
+    });
+  }
 }
